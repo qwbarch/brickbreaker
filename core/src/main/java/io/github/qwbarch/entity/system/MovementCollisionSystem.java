2920b1cbaa2123e6a4d30c76dc1251fe57dcee6a
qwbarch <qwbarch@gmail.com>
2025-07-30 15:13:06 -0400

    Reduce passes

--- core/src/main/java/io/github/qwbarch/entity/system/MovementCollisionSystem.java
+++ core/src/main/java/io/github/qwbarch/entity/system/MovementCollisionSystem.java
@@ -27,7 +27,7 @@ public final class MovementCollisionSystem extends LogicSystem {
      * The higher # of passes, the less likely tunneling happens.
      * However, the higher # of passes, the higher the cpu usage as well.
      */
-    private static final int PASSES = 20;
+    private static final int PASSES = 4;
     private static final float SECONDS_PER_PASS = 1f / PASSES;
 
     /**
